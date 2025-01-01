3581f5a085d44a8bbb41d5ee861dc7e22c5fabd6
Krzaczor <jbielecki18@gmail.com>
2025-01-01 13:35:09 +0100

    Updates

--- src/feature/day.ts
+++
@@ -1,9 +0,0 @@
-export type Day = number | null;
-
-interface RenderDay {
-    day: Day;
-}
-
-export const renderDay = ({ day }: RenderDay) => {
-    return `<div class="day">${day !== null ? day + 1 : ""}</div>`;
-};
